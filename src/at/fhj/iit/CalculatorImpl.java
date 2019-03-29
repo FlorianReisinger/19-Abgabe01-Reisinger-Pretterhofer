3825ad06e9b8f9a8a5ae06e6767cae5507a2319b
Florian Reisinger <florian.reisinger@edu.campus02.at>
2019-03-29 15:59:51 +0100

    Created class CalculatorImpl.java and added function(getMinimum(), getMaximum(), addValue())
    Created class CalculatorImpl.java with the structure of the interface "Calclulator.java"
    and implemented getMinimum(), getMaximum() and addValue(),
    which add a Integer-value to an Integer ArrayList.
    Also the ArrayList is created as an attribute of the class CalculatorImpl.
    The function getMinimum() and getMaximum() are just declared without content.

---
+++ src/at/fhj/iit/CalculatorImpl.java
@@ -0,0 +1,26 @@
+package at.fhj.iit;
+
+import java.util.*;
+
+public class CalculatorImpl {
+	//Attributes
+	private List<Integer> numbersList = new ArrayList<Integer>();
+	
+	//Methods
+	//-----------------------------------------------------------
+	
+	//Getter minimum
+	public int getMinimum() {
+	}
+	
+	//Getter maximum
+	public int getMaximum() {
+	}
+	
+	//Add a values to the Integer-List
+	public void addValue(int value) {
+		numbersList.add(value);
+	}
+	
+	
+}
